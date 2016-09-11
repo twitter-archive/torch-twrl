913e159e5af77bdeac96aa8c8ffa1369b1ee4dfb
Kory Mathewson <korymath@gmail.com>
2016-09-11 11:35:23 -0700

    testing parameters of network

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,9 @@ echo "*************************************************"
 
 # th testScript.lua -env 'MountainCar-v0' -policy random -learningUpdate noLearning -model noModel -nSteps 1000 -nIterations 500 -timestepsPerBatch 1000 -video 200 -renderAllSteps true
 
-th testScript.lua -env 'CartPole-v0' -policy categorical -learningUpdate reinforce -model singleHiddenLayerCategorical -nSteps 1000 -nIterations 500 -timestepsPerBatch 1000 -stepsizeStart 0.3 -gamma 0.9 -nHiddenLayerSize 10 -video 0 -gradClip 4 -baselineType padTimeDepAvReturn -renderAllSteps false -beta 0.01 -weightDecay 0.9 -windowSize 100 -uploadResults true
+th testScript.lua -env 'CartPole-v0' -policy categorical -learningUpdate \
+	reinforce -model singleHiddenLayerCategorical -nSteps 1000 \
+	-nIterations 500 -timestepsPerBatch 2000 -stepsizeStart 0.5 -gamma 1 \
+	-nHiddenLayerSize 20 -video 0 -gradClip 5 -baselineType padTimeDepAvReturn \
+	-renderAllSteps false -beta 0.01 -weightDecay 0.9 -windowSize 20
+	-uploadResults true
