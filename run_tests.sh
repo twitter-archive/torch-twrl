374303436beb26fa8d176f1bc0f5de77f6c3d32a
Kory Mathewson <korymath@gmail.com>
2016-09-11 10:50:09 -0700

    cleaning the performance metrics

--- run_tests.sh
+++ run_tests.sh
@@ -6,4 +6,4 @@ echo "*************************************************"
 
 # th testScript.lua -env 'MountainCar-v0' -policy random -learningUpdate noLearning -model noModel -nSteps 1000 -nIterations 500 -timestepsPerBatch 1000 -video 200 -renderAllSteps true
 
-th testScript.lua -env 'CartPole-v0' -policy categorical -learningUpdate reinforce -model singleHiddenLayerCategorical -nSteps 1000 -nIterations 500 -timestepsPerBatch 10 -stepsizeStart 0.3 -gamma 1 -nHiddenLayerSize 10 -video 200 -gradClip 5 -baselineType padTimeDepAvReturn -renderAllSteps true -beta 0.01 -weightDecay 0.9
+th testScript.lua -env 'CartPole-v0' -policy categorical -learningUpdate reinforce -model singleHiddenLayerCategorical -nSteps 1000 -nIterations 500 -timestepsPerBatch 1000 -stepsizeStart 0.3 -gamma 1 -nHiddenLayerSize 10 -video 200 -gradClip 5 -baselineType padTimeDepAvReturn -renderAllSteps true -beta 0.01 -weightDecay 0.9
