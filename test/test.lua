6e7a216eecc54a2774e3c9b8f406cf813b1672fa
Kory Mathewson <kmathewson@twitter.com>
2016-09-13 18:32:13 -0700

    adding testing across gym

--- test/test.lua
+++ test/test.lua
@@ -2,6 +2,17 @@ local testSuite = torch.TestSuite()
 local tester = torch.Tester()
 local gymClient = require '../src/gym-http-api/binding-lua/gym_http_client'
 
+local verbose = false
+local render = false
+local video_callable = 2
+
+local runTest = require '../src/gym-http-api/binding-lua/test_api'({
+   gymClient = gymClient, 
+   verbose = verbose,
+   render = render,
+   video_callable = video_callable
+})
+
 -- Load all 
 local util = require 'rl.util'()
 
@@ -11,19 +22,29 @@ function testSuite.torchTensorTest()
    tester:eq(a, b, 0.01, "a and b should be approximately equal")
 end
 
-function testSuite.testAPI()
-	local function testAPI()
-		local verbose = false
-		local runTest = require '../src/gym-http-api/binding-lua/test_api'({
-			gymClient = gymClient, 
-			verbose = verbose
-		})
-		local testEnvs = {'CartPole-v0', 'FrozenLake-v0'}
-		for i = 1,#testEnvs do
-			local success = runTest(testEnvs[i])
-		end
-	end
-   tester:assertNoError(testAPI, "testAPI shouldn't give an error")
+function testSuite.testCartPole()
+	local success = runTest('CartPole-v0')
+   tester:eq(success, true, "testCartPole shouldn't give an error")
+end
+
+function testSuite.testPendulum()
+   local success = runTest('Pendulum-v0')
+   tester:eq(success, true, "testPendulum shouldn't give an error")
+end
+
+function testSuite.testFrozenLake()
+   local success = runTest('FrozenLake-v0')
+   tester:eq(success, true, "testCartPole shouldn't give an error")
+end
+
+function testSuite.testAtari()
+   local success = runTest('BattleZone-v0')
+   tester:eq(success, true, "testAtari shouldn't give an error if you have Atari configured")
+end
+
+function testSuite.testMujoco()
+   local success = runTest('InvertedPendulum-v1')
+   tester:eq(success, true, "testMujoco shouldn't give an error if you have MuJoCo configured")
 end
 
 function testSuite.tilecode()
