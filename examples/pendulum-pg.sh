ed871fc5a2265e8059d7f8548ed82b954fe35a56
Arjun Maheswaran <amaheswaran@twitter.com>
2016-09-13 17:59:29 -0700

    Added examples

---
+++ examples/pendulum-pg.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+clear
+echo "REINFORCE on Pendulum Environment"
+echo "************************************"
+
+th run.lua \
+	-env 'Pendulum-v0' \
+	-policy normal \
+	-learningUpdate reinforce \
+  -model mlp \
+	-optimAlpha 0.9 \
+  -timestepsPerBatch 1000 \
+	-stepsizeStart 0.3 -gamma 1 \
+	-nHiddenLayerSize 10 \
+	-gradClip 5 \
+	-baselineType padTimeDepAvReturn \
+	-beta 0.01 \
+	-weightDecay 0 \
+	-windowSize 100 \
+  -nSteps 1000 \
+	-nIterations 500 \
+	-video 0 \
+	-uploadResults true \
+	-renderAllSteps false
--- src/init.lua
+++ src/init.lua
@@ -10,6 +10,7 @@ rl.util = require 'rl.util'
 rl.perf = require 'rl.perf'
 rl.tilecoding = require 'rl.tilecoding'
 rl.experiment = require 'rl.experiment'
+rl.gymClient = require 'rl.binding-lua.gym_http_client'
 
 -- Return package
 return rl
