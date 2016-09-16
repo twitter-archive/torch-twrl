69bab265f3e598b26d0e550e4ff4175214f6e03f
Kory Mathewson <korymath@gmail.com>
2016-09-15 18:11:43 -0700

    split test to unit and integration

--- test/test.lua
+++ test/test.lua
@@ -1,25 +1,8 @@
 local base = torch.TestSuite()
-local api = torch.TestSuite()
-local atari = torch.TestSuite()
-local mujoco = torch.TestSuite()
 local performance = torch.TestSuite()
 local tilecoding = torch.TestSuite()
-local experiment = torch.TestSuite()
 
 local tester = torch.Tester()
-local gymClient = require '../src/gym-http-api/binding-lua/gym_http_client'
-
--- disable rendering and video for testing
-local verbose = false
-local render = false
-local video_callable = 0
-
-local runTest = require '../src/gym-http-api/binding-lua/test_api'({
-   gymClient = gymClient, 
-   verbose = verbose,
-   render = render,
-   video_callable = video_callable
-})
 
 -- Load all 
 local util = require 'rl.util'()
@@ -28,31 +11,6 @@ function base.torchTensor()
    local a = {2, torch.Tensor{1, 2, 2}}
    local b = {2, torch.Tensor{1, 2, 2.001}}
    tester:eq(a, b, 0.01, "a and b should be approximately equal")
-end
-
-function api.testCartPole()
-   local success = runTest('CartPole-v0')
-   tester:eq(success, true, "testCartPole shouldn't give an error")
-end
-
-function api.testPendulum()
-   local success = runTest('Pendulum-v0')
-   tester:eq(success, true, "testPendulum shouldn't give an error")
-end
-
-function api.testFrozenLake()
-   local success = runTest('FrozenLake-v0')
-   tester:eq(success, true, "testCartPole shouldn't give an error")
-end
-
-function atari.testAtari()
-   local success = runTest('BattleZone-v0')
-   tester:eq(success, true, "testAtari shouldn't give an error if you have Atari configured")
-end
-
-function mujoco.testMujoco()
-   local success = runTest('InvertedPendulum-v1')
-   tester:eq(success, true, "testMujoco shouldn't give an error if you have MuJoCo configured")
 end
 
 function tilecoding.tilecodeConsistent()
@@ -117,30 +75,7 @@ function performance.getSummary()
    tester:eq(summary.windowSize, 10, "performance: get summary failed")
 end
 
-function experiment.badExperimentCall()
-   local performance = require 'rl.experiment'()
-   tester:eq(performance, {}, "bad experiment call should fail with bad settings ")
-end
-
-function experiment.randomNoLearningNoModel()
-   local env = 'CartPole-v0'
-   local params = {}
-   local agent = {
-      policy = 'random',
-      learningUpdate = 'noLearning',
-      model = 'noModel'
-   }
-   local nSteps = 2
-   local nIterations = 2
-   local performance = require 'rl.experiment'(env, agent, nSteps, nIterations, params)
-   tester:eq(performance.iteration, 2, "basic experiment should run")
-end
-
 tester:add(base)
-tester:add(api)
-tester:add(atari):disable('testAtari')
-tester:add(mujoco):disable('testMujoco')
-tester:add(performance)
 tester:add(tilecoding)
-tester:add(experiment)
+tester:add(performance)
 tester:run()
